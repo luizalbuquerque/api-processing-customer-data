61de1e91b12635ef81f07400172e8e0c95541a4b
08064120977 <luiz.info.2016@gmail.com>
2023-06-15 15:31:06 -0300

    Adding & Update application.properties - heathCheck - pom

---
+++ src/main/java/api/processingcustomerdata/controller/HealthCheckController.java
@@ -0,0 +1,23 @@
+package api.processingcustomerdata.controller;
+
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HealthCheckController {
+
+    @RequestMapping(value = "/api-processing-customer-data", method = RequestMethod.GET, produces = { "application/json"})
+    @Operation(description = "Request to intercept" , responses = {
+            @ApiResponse(description = "Request has been forwarded", responseCode = "200", content = @Content(mediaType = "application/json")),
+            @ApiResponse(description = "There was an error forwarding the request", responseCode = "500")})
+    public ResponseEntity<String> HealthCheck() {
+        return new ResponseEntity<>("I'm alive - Processing Customer Data!", HttpStatus.OK);
+    }
+
+}
