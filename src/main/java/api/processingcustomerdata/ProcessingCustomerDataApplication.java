291b8329649ce26f79655fbecb1adb15822bbf67
08064120977 <luiz.info.2016@gmail.com>
2023-06-15 15:23:16 -0300

    first commit

---
+++ src/main/java/api/processingcustomerdata/ProcessingCustomerDataApplication.java
@@ -0,0 +1,13 @@
+package api.processingcustomerdata;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProcessingCustomerDataApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProcessingCustomerDataApplication.class, args);
+	}
+
+}
---
+++ src/test/java/api/processingcustomerdata/ProcessingCustomerDataApplicationTests.java
@@ -0,0 +1,13 @@
+package api.processingcustomerdata;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProcessingCustomerDataApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
