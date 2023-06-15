506fd6c8f9875007bf6119bcdfa0805e8c0e05e4
08064120977 <luiz.info.2016@gmail.com>
2023-06-15 15:35:04 -0300

    Update Swagger and properties

--- src/main/java/api/processingcustomerdata/ProcessingCustomerDataApplication.java
+++ src/main/java/api/processingcustomerdata/ProcessingCustomerDataApplication.java
@@ -1,8 +1,11 @@
 package api.processingcustomerdata;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@OpenAPIDefinition(info = @Info(title = "API Processing Customer Data", version = "1.0.0"))
 @SpringBootApplication
 public class ProcessingCustomerDataApplication {
 
